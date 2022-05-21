fe431b3623e1d04352c892ae608fc8f68e5087ab
Aschweik766 <ashnjackson766@gmail.com>
2022-05-21 15:03:06 -0600

    skeleton user routes created get, post

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,49 @@
 //User homepage: routes//
-
 const express = require('express');
-const res = require('express/lib/response')
-//const User = require('..models/user-model')
+
+const User = require('../models/user-model')
 
 const router = express.Router();
 
 //CRUD ROUTES//
 
-append.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.send('hello world')
 })
+
+//CREATE 
+
+//READ
+router.get("/user", (req, res) => {
+    const results = User.find({})
+    results.then((user) => {res.send(user)})
+})
+
+//other routes to plan//
+
+//POST
+
+router.post("/", (req, res) => {
+    User.create(req.body)
+    .then( () => {
+        res.redirect('/')
+    })
+    .catch(console.error)
+})
+
+
+//UPATE
+
+// router.put()
+
+// router.delete()
+
+
+
+
+
+
+
 
 module.exports = router;
 
