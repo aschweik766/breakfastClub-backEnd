e8d3d4d1e0781112840681a9887c10fd1102abb1
Aschweik766 <ashnjackson766@gmail.com>
2022-06-03 10:42:26 -0600

    update and remove cors.

--- models/user-model.js
+++ models/user-model.js
@@ -26,7 +26,7 @@ const UserSchema = new mongoose.Schema(
         weight: String,
         interests: [String],
         matches: [
-            {user_id: String}
+            {username: String}
         ]
     },
     {timestamps: true}
