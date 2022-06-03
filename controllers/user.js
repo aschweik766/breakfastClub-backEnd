b08c6f92316a7f0400ed038d7e966e82c360e15f
Aschweik766 <ashnjackson766@gmail.com>
2022-06-03 17:47:25 -0600

    updated scratch, updated add/match route with _id

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,9 @@
-//User homepage: routes//
 const express = require('express');
 
 const User = require('../models/user-model')
 
-// const { request } = require('express');
-// const query = require('express/lib/middleware/query');
+const { request } = require('express');
+const query = require('express/lib/middleware/query');
 
 const router = express.Router();
 
@@ -13,7 +12,6 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.send('hello world')
 })
-
 
 //READ
 router.get("/users", (req, res) => {
@@ -26,32 +24,19 @@ router.get("/users/:id", (req, res) => {
 })
 
 
+//getting a single user by id only for login matches might user of DisplayMatchesFrontEnd, not using now.
+// router.get("/login-user/:id", (req, res) => {
+//     const result = User.findById(req.params.id)
+//     result.then((user) => { res.send(user) })
+// })
+
+
 router.get("/users-gender-identity", (req, res) => {
     const result = User.find({genderIdentity: {$eq: 'Male'}})
     result.then((user) => { res.send(user) })
 
 })
 
-// router.get("/users-gender-identity", (req, res) => {
-//     const gender = req.query.gender
-//     const query = { genderIdentity : { $eq: 'Male' } }
-
-//     const findUsersByGender = User.find(query).toArray()
-
-//     res.send(findUsersByGender)
-// })
-//other routes to plan//
-
-//CREATE 
-//POST
-
-// router.post("/users", (req, res) => {
-//     User.create(req.body)
-//         .then(() => {
-//             res.redirect('/myaccount')
-//         })
-//         .catch(console.error)
-// })
 
 router.post("/signup", (req, res) => {
     console.log("signup")
@@ -87,7 +72,7 @@ router.post("/signup", (req, res) => {
 })
 
 
-//UPDATE
+//UPDATE edit profile:
 
 router.put('/users/:id', (req, res) => {
     console.log(req.params.id)
@@ -97,50 +82,167 @@ router.put('/users/:id', (req, res) => {
         console.log("user updated")
             })
 
+//updating/add matches to a user_id individual dashboard:
 
+router.put('/update-matches', async (req, res) => {
+        const { loginUsername, matchedUsername } = req.body
+        try {
+            const query = { username: loginUsername }
+            const updateMatches = { $push: { matches: { username: matchedUsername }},
+            }
+            const user = await User.findOneAndUpdate(query, updateMatches)
+            res.send(user)
+        } catch {
+            console.log(error)
+        }
+    })
 
-//updating/add matches to a user_id individual dashboard
-
-// router.put('/update-matches', async (req, res) => {
-//         const { loginUserId, matchedLoginId } = req.body
-//         try {
-//             const query = { user_id: loginUserId }
-//             const updateMatches = { $push: { matches: { user_id: matchedLoginId }},
-//             }
-//             const user = await User.findOneAndUpdate(query, updateMatches)
-//             res.send(user)
-//         } catch {
-//             console.log(error)
-//         }
-        
-    //     const results = User.findByIdAndUpdate({_id: user_id}, 
-    //     {$push: {matches: {user_id: matchedIds}}})
-    //    results.then((user) => { res.send(user) })
-    
-// })
-
-// DELETE
+// DELETE did work on HTTP test route with backend.
 
 router.delete('/myaccount/:id', (req, res) => {
     User.findByIdAndDelete(req.params.id)
         .catch(console.error)
 })
-//will have to update backend to matches, update User with matches, update User with messages, get user by compatability: 
-    //router.put(user/:addMatches)
-    //router.get(user/:matches)
-    //router.get(user/:compatability)
-    //router.get(user/:get-messages)
-        //{from user_id to a user_id}
-    //router.post(user/:send-message)
-
-
-
-
-
-
 
 
 
 module.exports = router;
+
+
+
+
+
+//scratch
+
+
+// router.get("/users-gender-identity", (req, res) => {
+//     const gender = req.query.gender
+//     const query = { genderIdentity : { $eq: 'Male' } }
+
+//     const findUsersByGender = User.find(query).toArray()
+
+//     res.send(findUsersByGender)
+// })
+//other routes to plan//
+
+//create post route to Signup a user:
+
+// delete tester//
+// router.delete('/myaccount/:id', (req, res) => {
+//     User.findOneAndRemove(
+//         {_id: req.params.id},
+//         )
+//         .then( () => res.redirect('/'))
+//         .catch(console.error)
+//   })
+
+
+// router.post("/users", (req, res) => {
+//     User.create(req.body)
+//         .then(() => {
+//             res.redirect('/myaccount')
+//         })
+//         .catch(console.error)
+// })
+
+
+//update/match array  tester
+
+// router.put('/update-matches/:loginUsername', (req, res) => {
+    
+//     User.findOneAndUpdate({username: req.params.loginUsername}, req.body )
+//     .then((data) => res.json(data))
+//         console.log("user matches updated.")
+// })
+
+
+// //User homepage: routes//
+// const express = require('express');
+
+// const User = require('../models/user-model')
+// const SignUp = require('../models/signUpModel');
+// const { request } = require('express');
+
+// const router = express.Router();
+
+// //CRUD ROUTES//
+
+// router.get("/", (req, res) => {
+//     res.send('hello world')
+// })
+
+// //CREATE 
+
+// //READ
+// router.get("/users", (req, res) => {
+//     const results = User.find({})
+//     results.then((user) => {res.send(user)})
+// })
+// router.get("/users/:id", (req, res) => {
+//     const result = User.findById(req.params.id)
+//     result.then((user) => {res.send(user)})
+// })
+
+// //other routes to plan//
+
+// //POST
+
+// router.post("/users", (req, res) => {
+//     User.create(req.body)
+//     .then( () => {
+//         res.redirect('/myaccount')
+//     })
+//     .catch(console.error)
+// })
+
+// router.post('/signup', (req, res) => {
+//     const signedUpUser= new SignUp({
+//         fullName: req.body.fullName,
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: req.body.password
+//     })
+//     signedUpUser.save()
+//     .then(data => {
+//         res.json(data)
+//     })
+//     .catch(error => {
+//         res.json(error)
+//     })
+// })
+
+
+// //UPATE
+
+
+// router.put('/users/:id', (req, res) => {
+//     console.log(req.params.id)
+//     console.log(req.body)
+//     User.findByIdAndUpdate({_id: req.params.id}, req.body)
+//     .then(data => 
+//         User.find({}).then(data => {
+//             res.json(data)
+//         }))
+// })
+
+
+
+
+// // DELETE
+
+// router.delete('/myaccount/:id', (req, res) => {
+//     User.findByIdAndDelete(req.params.id)
+//     .catch(console.error)
+// })
+
+
+
+
+
+
+
+
+// module.exports = router;
+
 
 
